0541bd17c55ce47e6606c736e565b5114a70312c
TyFangXV <68788849+TyFangXV@users.noreply.github.com>
2021-06-09 21:31:09 +0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,5 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particle-div', '../json/particlesjs-config.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+  
